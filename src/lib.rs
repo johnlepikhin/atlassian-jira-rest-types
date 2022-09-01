d536250d0d85f59bdf764c8cbea64f6c77ab7a16
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2022-09-01 14:36:43 +0300

    feat: Added API v3

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,14 @@ extern crate schemafy_core;
 extern crate serde;
 extern crate serde_json;
 
-use serde::{Deserialize, Serialize};
+pub mod v2 {
+    use serde::{Deserialize, Serialize};
 
-schemafy::schemafy!("src/schema.json");
+    schemafy::schemafy!("src/schema-v2.json");
+}
+
+pub mod v3 {
+    use serde::{Deserialize, Serialize};
+
+    schemafy::schemafy!("src/schema-v3.json");
+}
